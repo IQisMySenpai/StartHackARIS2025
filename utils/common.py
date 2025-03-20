0148554ea9d3d76fd4dc97730a2f09760d8fa9c2
Jannick <jannick.schroer@gmail.com>
2025-03-20 08:09:56 +0100

    Added Writing Duration Function

--- utils/common.py
+++ utils/common.py
@@ -1,8 +1,18 @@
 import json
 import os
+from random import uniform
 
 def config_load():
     # Load API Key from Config Json
     with open(os.path.join(os.path.dirname(__file__), "../config/config.json")) as f:
         data = json.load(f)
         return data
+
+def generate_writing_duration(msg):
+    msg_length = len(msg)
+    avg_chars_per_s = 0.8 + uniform(-0.1, 0.1)
+
+    duration = (msg_length / avg_chars_per_s) + uniform(-2, 2)
+
+    return duration
+
