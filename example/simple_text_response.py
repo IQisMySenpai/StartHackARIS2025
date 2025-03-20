aba3bed0100ad5147d671cafdd26898758b4bab3
Jannick <jannick.schroer@gmail.com>
2025-03-20 02:54:58 +0100

    Continued integration of multiple users and whitelist

--- example/simple_text_response.py
+++ example/simple_text_response.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from openai import OpenAI
 import json
 import requests
+from time import sleep
+from random import uniform
 
 # Load API Key from Config Json
 with open('../config/config.json') as f:
@@ -14,41 +16,101 @@ client = OpenAI(
 
 app = Flask(__name__)
 
+messages = {}
+
+whitelist = [
+    '13233779601@s.whatsapp.net',
+    '41774063608@s.whatsapp.net'
+]
+
+def change_state(wa_id, state):
+    payload = {
+        "wa_id": wa_id,
+        "presence": state
+    }
+    headers = {'Content-Type': 'application/json'}
+
+    try:
+        response = requests.post('http://localhost:3000/fuck-waha/set-presence', json=payload, headers=headers)
+        if response.status_code == 200:
+            return True
+    except requests.RequestException as e:
+        print(e)
+    return False
+
+def send_message(wa_id, message):
+    payload = {
+        "wa_id": wa_id,
+        "message": message
+    }
+    headers = {'Content-Type': 'application/json'}
+
+    try:
+        response = requests.post('http://localhost:3000/fuck-waha/send-message', json=payload, headers=headers)
+        if response.status_code == 200:
+            return True
+    except requests.RequestException as e:
+        print(e)
+    return False
+
+def chat_request(wa_id, message):
+    global messages
+
+    if wa_id not in messages:
+        messages[wa_id] = [{
+            "role": "developer",
+            "content": "Write as if you were the girlfriend of Matias. Say something flirty and cute."
+        }]
+
+    messages[wa_id].append({
+        "role": "user",
+        "content": f'{message}'
+    })
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini-2024-07-18",
+        messages=messages[wa_id]
+    )
+
+    response = completion.choices[0].message.content
+
+    messages[wa_id].append({
+        'content': f'{response}',
+        'role': 'assistant'
+    })
+
+    return response
+
+
 @app.route('/wh/dummy', methods=['POST'])
 def webhook():
+    global messages
     if request.method == 'POST':
         data = request.json  # Get the JSON data sent in the request
 
         try:
-            message = data['message']['extendedTextMessage']['text']
+            user = data['key']['remoteJid']
 
-            completion = client.chat.completions.create(
-                model="gpt-4o-mini-2024-07-18",
-                messages=[{
-                    "role": "user",
-                    "content": f'Write a one-sentence response to "{message}"'
-                }]
-            )
+            if user not in whitelist:
+                print(f'Got Message from {user}, which is not whitelisted')
+                return jsonify({'message': 'Data received successfully'}), 200
 
-            response_message = completion.choices[0].message.content
+            if 'extendedTextMessage' in data['message']:
+                message = data['message']['extendedTextMessage']['text']
+            else:
+                message = data['message']['conversation']
 
-            # Send the response message to the specified endpoint
-            payload = {
-                "wa_id": data['key']['remoteJid'],
-                "message": response_message
-            }
-            headers = {'Content-Type': 'application/json'}
+            print(f'Received {message} from {user}')
 
-            try:
-                response = requests.post('http://localhost:3000/fuck-waha/send-message', json=payload, headers=headers)
-                if response.status_code == 200:
-                    print('Message sent successfully!')
-                else:
-                    print(f'Failed to send message. Status code: {response.status_code}, Response: {response.text}')
-            except requests.RequestException as e:
-                print(f'Request failed: {e}')
+            change_state(user, 'composing')
 
-            return jsonify({'message': 'Data received and processed successfully!'}), 200
+            response = chat_request(user, message)
+
+            sleep(uniform(1, 5))
+            change_state(user, 'paused')
+            send_message(user, response)
+
+            return jsonify({'message': 'Data received successfully'}), 200
         except KeyError:
             return jsonify({'message': 'Data received successfully, but no text message found!'}), 200
 
